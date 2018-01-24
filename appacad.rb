c327c33f1c4ef15ba7f729b5fe6f95a95f94057d
Bijaya Manandhar <bijayamanandhar@Bijayas-MacBook-Pro.local>
2018-01-24 13:57:55 -0800

    some change added

--- appacad.rb
+++ appacad.rb
@@ -78,3 +78,18 @@ city = gets.chomp
 
 print "What state or province are you from? "
 state = gets.chomp
+print "What's your first name? "
+first_name = gets.chomp
+puts "Your name is #{first_name}!"
+
+print "What's your last name? "
+last_name = gets.chomp
+puts "Your last name is #{}!"
+
+print "Which city are you from? "
+city = gets.chomp
+puts "You are from #{city}"
+
+print "What state or province are you from? "
+state = gets.chomp
+puts "#{state}"
