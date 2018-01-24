cb80388bfe6eb783e564c910ca35ae192cafe263
Bijaya Manandhar <bijayamanandhar@Bijayas-MacBook-Pro.local>
2018-01-24 12:42:12 -0800

    this is an example of data types

--- appacad.rb
+++ appacad.rb
@@ -1,3 +1,13 @@
 x = 'Name Field = > Bijaya'
 print x
 ===
+# Welcome to Ruby!
+my_num = 25    # Add your code here!
+
+my_boolean = true    # And here!
+
+my_string = "Ruby"    # Also here.
+
+puts my_num
+puts my_boolean
+puts my_string
